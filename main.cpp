edaa24989c5a3ece5165a11a2a6bfbc1f853d04d
lav1sh <lavishkumar@outlook.in>
2026-01-09 22:19:37 +0530

    updates

--- main.cpp
+++ main.cpp
@@ -6,92 +6,27 @@ const int MOD = 1e9+7;
 
 // Observations
 /*
-op -- 
-    pick i where i<=i+1
-    okay so i cant pick the last element at n-1
-    i can increase
 
-5 6
-1 3 6 5 1
-
-1 3 6 5 1
-can i pick 5 no, 4 yes make it 6 = 2 operation
-
-1 3 6 5 1
-1 7 6 5 1 === 4op k==0 now , max==7
-
-
-5 6
-6 5 4 1 5
-
-basically pick the largest element which is not the first == 5 and 5
-1 to 6 in 5 op
-if(i<=i+1) i can be at max i+1+1
-
-
-so basically you try to equalize them and spread
-7 6 5 4 5 
-
-5 6
-
-can we make 7
-6 5 4 1 5 
-7 6 5 4 3   diff = 1+1+1+3+0 = 6<=k done
-we can try for all positions as the time limit is larger n square , popb n^2logn is good
-make 8
-
-
-1 to 6, or just do 1 to 4
-can be make some ans, thats the question then we can bs
 
 
 
 */
-vector<int> arr;
-
-bool check(int x,int k){
-
-    int n=arr.size();
-    for(int i=0;i<n-1;++i){
-        // make arr[i]==x rest x-2,x-2,x-3 and so on
-        int req_k=0;
-        int req_num=x;
-        for(int j=i;j<n-1;++j){
-            if(arr[j]>req_num) continue;
-            req_k+=(req_num-arr[j]);
-            req_num--;
-        }
-        if(req_k<=k) return true;
-    }
-    return false;
-}
 
 
 void solve(){
-    int n,k;cin>>n>>k;
-    arr.assign(n,0);
-    for(int i=0;i<n;++i) cin>>arr[i];
-    
-    int i=0,j=1e8+1;
-    
-    while(j>i){
-        int mid=(i+j)/2;
-
-        if(check(mid,k)) i=mid;
-        else j=mid-1;
-    }
-
-    cout<<i<<endl;
-
-
 
 }
 
 int32_t main(){
+
+#ifdef lav1sh
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
     int t; cin >> t;
     while(t--) solve();
 
